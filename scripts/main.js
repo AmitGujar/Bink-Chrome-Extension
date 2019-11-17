a7266b4abb17a1f8698cf2a97910482459fb3fc1
Amit Dilip Gujar <amithero3342@gmail.com>
2019-11-18 00:49:08 +0530

    build 2.0.1

--- scripts/main.js
+++ scripts/main.js
@@ -111,8 +111,10 @@ function checkTimeAddZero(i) {
 window.addEventListener("load", () => {
   let long;
   let lat;
-  let temperatureDescription = document.querySelector('.temperature-description');
-  let temperatureDegree = document.querySelector('.temperature-degree');
+  let temperatureDescription = document.querySelector(".temperature-description");
+  let temperatureDegree = document.querySelector(".temperature-degree");
+  let temperatureSection = document.querySelector(".temperature");
+  const temperatureSpan = document.querySelector(".unit");
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
       long = position.coords.longitude;
@@ -127,12 +129,32 @@ window.addEventListener("load", () => {
         .then(data => {
           const {
             temperature,
-            summery,
+            summary,
             icon
           } = data.currently;
-          temperatureDegree.textContent = temperature;
-          temperatureDescription.textContent = summery;
+          temperatureDegree.textContent = Math.floor(temperature);
+          temperatureDescription.textContent = summary;
+          let celcius = (temperature - 32) * (5 / 9);
+          setIcons(icon, document.querySelector('.icon'));
+          temperatureSection.addEventListener('click', () => {
+            if (temperatureSpan.textContent === "°F") {
+              temperatureSpan.textContent = "°C";
+              temperatureDegree.textContent = Math.floor(celcius);
+            } else {
+              temperatureSpan.textContent = "°F";
+              temperatureDegree.textContent = Math.floor(temperature);
+            }
+          })
         });
     });
+  }
+
+  function setIcons(icon, iconID) {
+    const skycons = new Skycons({
+      color: "white"
+    });
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+    skycons.play();
+    return skycons.set(iconID, Skycons[currentIcon]);
   }
 });
