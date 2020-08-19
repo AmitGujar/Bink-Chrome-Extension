96de760fa4a5ee334623ce687a5756e03009effe
Amit Dilip Gujar <amithero3342@gmail.com>
2020-08-20 02:19:32 +0530

    bug fixed

--- scripts/main.js
+++ scripts/main.js
@@ -5,20 +5,20 @@ window.onload = function () {
   });
 };
 
-window.addEventListener('load', function () {
-  console.log('All assets are loaded')
+window.addEventListener("load", function () {
+  console.log("All assets are loaded");
   var searchButton = document.getElementById("search-btn");
   searchButton.addEventListener("click", function () {
     var searchQuery = document.getElementById("input-query").value;
     var searchURL;
-    if (searchQuery === '') {
+    if (searchQuery === "") {
       alert("Oooh..!! Enter Something");
     } else {
       searchURL = "https://www.bing.com/search?q=" + searchQuery;
       window.open(searchURL);
       window.close();
     }
-  })
+  });
 
   var input = document.getElementById("input-query");
   input.addEventListener("keyup", function (event) {
@@ -27,8 +27,7 @@ window.addEventListener('load', function () {
       document.getElementById("search-btn").click();
     }
   });
-
-})
+});
 
 function init() {
   fetchImage();
@@ -37,21 +36,26 @@ function init() {
 }
 
 //api access key
-const clientID = "?client_id=8249d860403116cd4d1f60d039c9decb42300d417588a4d5960e8630f47a14cf";
+const clientID =
+  "?client_id=8249d860403116cd4d1f60d039c9decb42300d417588a4d5960e8630f47a14cf";
 
 function getQuotes() {
   fetch("https://quote-garden.herokuapp.com/quotes/random")
-    .then(res => res.json())
-    .then(data => {
-      document.querySelector('#quotes').innerHTML = `<h5 style='font-size:25px'>"${data.quoteText}"</h4><h5 style='font-size:20px'><i> by - ${data.quoteAuthor}</i></h5>`;
-    })
+    .then((res) => res.json())
+    .then((data) => {
+      document.querySelector(
+        "#quotes"
+      ).innerHTML = `<h5 style='font-size:25px'>"${data.quoteText}"</h4><h5 style='font-size:20px'><i> by - ${data.quoteAuthor}</i></h5>`;
+    });
 }
 
 function fetchImage() {
   const img = new Image();
   document.getElementById("background-container").style.opacity = 0;
   img.onload = function () {
-    document.getElementById("background-container").style.backgroundImage = `url(${this.src})`;
+    document.getElementById(
+      "background-container"
+    ).style.backgroundImage = `url(${this.src})`;
     document.getElementById("background-container").style.opacity = 1;
     document.getElementById("background-container").style.opacity = 1;
   };
@@ -61,14 +65,16 @@ function fetchImage() {
   } else {
     img.src = localStorage.getItem("url");
     credit.innerHTML = `<a target="_blank">${localStorage.getItem("name")}</a>`;
-    navigate.innerHTML = `<a target="_blank"  style="color : white; font-size:130%;" href="${localStorage.getItem("link")}">Download Photo</a>`;
+    navigate.innerHTML = `<a target="_blank"  style="color : white; font-size:130%;" href="${localStorage.getItem(
+      "link"
+    )}">Download Photo</a>`;
   }
 }
 
 function unsplashGetPhotos() {
   fetch(`https://api.unsplash.com/photos/random${clientID}`)
-    .then(res => res.json())
-    .then(data => {
+    .then((res) => res.json())
+    .then((data) => {
       localStorage.setItem("url", data.urls.full);
       localStorage.setItem("name", data.user.name);
       localStorage.setItem("link", data.links.html);
@@ -78,9 +84,21 @@ function unsplashGetPhotos() {
     })
     .catch((err) => {
       console.error(err);
-    })
+    });
 }
 
+// ! test code starts from here
+
+chrome.alarms.create("ChangeWallpaper", {
+  // delayInMinutes: 1.0,
+  periodInMinutes: 480,
+});
+
+chrome.alarms.onAlarm.addListener(function (alarm) {
+  unsplashGetPhotos(alarm);
+});
+
+//! test code ends here
 
 function getTime() {
   var systemDate = new Date();
@@ -108,35 +126,34 @@ function checkTimeAddZero(i) {
   }
   return i;
 }
+
 window.addEventListener("load", () => {
   let long;
   let lat;
-  let temperatureDescription = document.querySelector(".temperature-description");
+  let temperatureDescription = document.querySelector(
+    ".temperature-description"
+  );
   let temperatureDegree = document.querySelector(".temperature-degree");
   let temperatureSection = document.querySelector(".temperature");
   const temperatureSpan = document.querySelector(".unit");
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(position => {
+    navigator.geolocation.getCurrentPosition((position) => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
 
-      const proxy = 'http://cors-anywhere.herokuapp.com/';
+      const proxy = "http://cors-anywhere.herokuapp.com/";
       const api = `${proxy}https://api.darksky.net/forecast/551f03febb4552249ba0f312ebdbd498/${lat},${long}`;
       fetch(api)
-        .then(response => {
+        .then((response) => {
           return response.json();
         })
-        .then(data => {
-          const {
-            temperature,
-            summary,
-            icon
-          } = data.currently;
+        .then((data) => {
+          const { temperature, summary, icon } = data.currently;
           temperatureDegree.textContent = Math.floor(temperature);
           temperatureDescription.textContent = summary;
           let celcius = (temperature - 32) * (5 / 9);
-          setIcons(icon, document.querySelector('.icon'));
-          temperatureSection.addEventListener('click', () => {
+          setIcons(icon, document.querySelector(".icon"));
+          temperatureSection.addEventListener("click", () => {
             if (temperatureSpan.textContent === "°F") {
               temperatureSpan.textContent = "°C";
               temperatureDegree.textContent = Math.floor(celcius);
@@ -144,14 +161,14 @@ window.addEventListener("load", () => {
               temperatureSpan.textContent = "°F";
               temperatureDegree.textContent = Math.floor(temperature);
             }
-          })
+          });
         });
     });
   }
 
   function setIcons(icon, iconID) {
     const skycons = new Skycons({
-      color: "white"
+      color: "white",
     });
     const currentIcon = icon.replace(/-/g, "_").toUpperCase();
     skycons.play();
